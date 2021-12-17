93b6d3a311b94b42c2c52f41d026ad696b4a8773
Christopher <chrischurch21@gmail.com>
2021-12-17 16:34:13 -0500

    removed unneeded code from the project file

--- ConnectingThruMusic/Controllers/SurveyResultsController.cs
+++ ConnectingThruMusic/Controllers/SurveyResultsController.cs
@@ -16,28 +16,7 @@ namespace StaffMembers.Controllers
             return View(results);
         }
 
-        [HttpPost]
-        public FileResult Export()
-        {
-            List<SurveyResults> entities = Database.GetResults();
-            List<object> results = entities.ToList<object>();
 
-            results.Insert(0, new string[1] { " " });
-
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < results.Count; i++)
-            {
-                string[] result = (string[])results[i];
-                for (int j = 0; j < result.Length; j++)
-                {
-                    sb.Append(result[j] + ',');
-                }
-                sb.Append("\r\n");
-
-            }
-
-            return File(Encoding.UTF8.GetBytes(sb.ToString()), "text/csv", "Grid.csv");
-        }
     }
 }
-}
+
