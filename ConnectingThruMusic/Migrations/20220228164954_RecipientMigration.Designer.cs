790073347483f7bc7194cf0628dff5583b8e673c
David T <n01106181@unf.edu>
2022-02-28 11:55:47 -0500

    email form update

--- ConnectingThruMusic/Migrations/20220228164954_RecipientMigration.Designer.cs
+++ ConnectingThruMusic/Migrations/20220228164954_RecipientMigration.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StaffMembers;
 
 namespace StaffMembers.Migrations
 {
     [DbContext(typeof(ctmsurveyContext))]
-    partial class ctmsurveyContextModelSnapshot : ModelSnapshot
+    [Migration("20220228164954_RecipientMigration")]
+    partial class RecipientMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ConnectingThruMusic/Models/Recipient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaffMembers
+{
+    public class Recipient
+    {
+        [Key]
+        public int RecipientID { get; set; }
+        public string RecipientType { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string ChildName { get; set; }
+        public int sessionID { get; set; }
+        public int siteID { get; set; }
+    }
+}
