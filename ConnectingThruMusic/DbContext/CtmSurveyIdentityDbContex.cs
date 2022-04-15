9ec7102f8bfbe1d9bda11c5cf184046999dd5d71
Michael Mann <MMann@suddath.com>
2022-04-15 16:54:24 -0400

    Initial merge of IdentityDbContext

---
+++ ConnectingThruMusic/DbContext/CtmSurveyIdentityDbContex.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+
+
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace StaffMembers
+{
+    public partial class CtmSurveyIdentityDbContext : IdentityDbContext 
+   // public partial class ctmsurveyContext : DbContext
+
+    {
+        public CtmSurveyIdentityDbContext(DbContextOptions<CtmSurveyIdentityDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+           
+        }
+
+        
+    }
+}
+
