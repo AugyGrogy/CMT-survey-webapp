30b7c41aebb0ad69f650c0961b036cda241ff6f1
Tyler Grogg <n01304313@unf.edu>
2022-06-16 11:14:20 -0400

    start of display survey

---
+++ ConnectingThruMusic/Controllers/DisplaySurveyController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StaffMembers.Models;
+
+
+namespace StaffMembers.Controllers
+{
+    public class DisplaySurveyController : Controller
+    {
+        private readonly ctmsurveyContext _context;
+
+        public DisplaySurveyController(ctmsurveyContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> SurveyIndex()
+        {
+            return View(await _context.Survey.ToListAsync());
+        }
+
+        //gets infoirmation of survey from db
+        public async Task<IActionResult> DisplaySurvey(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var survey = await _context.Survey.FirstOrDefaultAsync(m => m.SurveyID == id);
+            if (survey == null)
+            {
+                return NotFound();
+            }
+            return View(survey);
+        }
+    }
+}
---
+++ ConnectingThruMusic/Models/DisplaySurveyViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaffMembers.Models
+{
+   public class DisplaySurveyViewModel
+    {
+        public int SurveyID { get; set; }
+        public string SurveyName { get; set; }
+        public string SurveyType { get; set; }
+        public List<int> SelectedQuestionId { get; set; }
+        public int QuestionId { get; set; }
+        public string QuestionText { get; set; }
+        public string QuestionType { get; set; }
+        public string AnswerA { get; set; }
+        public string AnswerB { get; set; }
+        public string AnswerC { get; set; }
+        public string AnswerD { get; set; }
+        public string AnswerE { get; set; }
+    }
+}
